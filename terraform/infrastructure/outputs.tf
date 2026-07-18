3786f527e68bd8f34acae3f4fcd56181a7a05b86
Baibhav Dhungana <baibhav.dhungana@truenary.com>
2026-07-18 22:57:09 +0545

    fix: configure endpoint and port as env varaibles

--- terraform/infrastructure/outputs.tf
+++ terraform/infrastructure/outputs.tf
@@ -5,3 +5,11 @@ output "ecr_repository_urls" {
     result = aws_ecr_repository.result-repository.repository_url
   }
 }
+
+output "rds_endpoint" {
+  value = aws_db_instance.main.address
+}
+
+output "redis_endpoint" {
+  value = aws_elasticache_cluster.main.cache_nodes[0].address
+}
