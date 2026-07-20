00049eb837e9fbc56a6bad163f81bc2c5e39c71c
Baibhav Dhungana <baibhav.dhungana@truenary.com>
2026-07-21 00:35:21 +0545

    fix: set the principle arn to user arn

--- terraform/infrastructure/eks.tf
+++ terraform/infrastructure/eks.tf
@@ -43,12 +43,12 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_eks_access_entry" "admin" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = data.aws_caller_identity.current.arn
+  principal_arn = "arn:aws:iam::${var.aws_account}:user/bav-admin-user"
 }
 
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name  = aws_eks_cluster.main.name
-  principal_arn = data.aws_caller_identity.current.arn
+  principal_arn = "arn:aws:iam::${var.aws_account}:user/bav-admin-user"
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
