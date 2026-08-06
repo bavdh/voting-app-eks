f746b1a5b84ae982f3fb7a351245bb0f74935e1c
Baibhav Dhungana <baibhav.dhungana@truenary.com>
2026-08-06 22:28:05 +0545

    fix: stylesheet path for index html file

--- result/server.js
+++ result/server.js
@@ -19,11 +19,11 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  host: process.env.POSTGRES_HOST || 'db',
+  host: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.POSTGRES_PORT || 5432,
   user: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB || 'postgres',
+  database: process.env.POSTGRES_DB || 'votingapp',
   ssl: {
     rejectUnauthorized: false
   }
